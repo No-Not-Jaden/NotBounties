1160818242c4f43f5e212fcf5b083d30886e2d09
Jaden Paterson <jpater567@gmail.com>
2024-04-09 14:57:55 -0500

    changed iso code request

--- src/main/java/me/jadenp/notbounties/utils/externalAPIs/LocalTime.java
+++ src/main/java/me/jadenp/notbounties/utils/externalAPIs/LocalTime.java
@@ -1,21 +1,27 @@
 package me.jadenp.notbounties.utils.externalAPIs;
 
-import com.maxmind.geoip2.WebServiceClient;
-import com.maxmind.geoip2.exception.GeoIp2Exception;
-import com.maxmind.geoip2.model.CityResponse;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import me.jadenp.notbounties.NotBounties;
 import me.jadenp.notbounties.utils.configuration.ConfigOptions;
 import me.jadenp.notbounties.utils.configuration.NumberFormatting;
+import org.apache.http.HttpEntity;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Authenticator;
 import java.net.InetSocketAddress;
-import java.net.PasswordAuthentication;
-import java.net.URL;
 import java.text.DateFormat;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
@@ -25,26 +31,25 @@ public class LocalTime {
 
     // these codes will only be saved in RAM and will be deleted after the plugin is disabled
     private static final Map<UUID, String> savedIsoCodes = new HashMap<>();
+    private static int account;
+    private static String license = null;
 
-    private static WebServiceClient client = null;
-
-    private static void registerClient() throws IOException {
+    private static void readAuthentication() throws IOException {
         // checks if secret file is present
         if (NotBounties.getInstance().getResource("secret.yml") == null)
             throw new IOException("No Secret File Found!");
         YamlConfiguration configuration = YamlConfiguration.loadConfiguration(new InputStreamReader(Objects.requireNonNull(NotBounties.getInstance().getResource("secret.yml"))));
         // load account id and license key from secret file
-        int account = configuration.getInt("geoip2.account");
-        String license = configuration.getString("geoip2.license");
-        client = new WebServiceClient.Builder(account, license).build();
+        account = configuration.getInt("geoip2.account");
+        license = configuration.getString("geoip2.license");
     }
 
     public static String formatTime(long time, Player player) {
         if (!ConfigOptions.autoTimezone)
             return formatTime(time);
-        if (client == null) {
+        if (license == null) {
             try {
-                registerClient();
+                readAuthentication();
             } catch (IOException e) {
                 return formatTime(time);
             }
@@ -69,15 +74,41 @@ public class LocalTime {
     }
 
     private static CompletableFuture<String> getIsoCode(InetSocketAddress address) {
+        if (license == null)
+            return CompletableFuture.failedFuture(new RuntimeException("No Authentication Credentials!"));
         return CompletableFuture.supplyAsync(() -> {
-            Authenticator.setDefault (new Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication ("username", "password".toCharArray());
-                }
-            });
-            URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid);
 
-            return cityResponse.getCountry().getIsoCode();
+            HttpGet request = new HttpGet("https://geolite.info/geoip/v2.1/city/" + address.getAddress() + "?pretty");
+
+            CredentialsProvider provider = new BasicCredentialsProvider();
+            provider.setCredentials(
+                    AuthScope.ANY,
+                    new UsernamePasswordCredentials( account + "", license)
+            );
+
+            try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+                    .setDefaultCredentialsProvider(provider)
+                    .build();
+                 CloseableHttpResponse response = httpClient.execute(request)) {
+
+                // 401 if wrong user/password
+                Bukkit.getLogger().info(response.getStatusLine().getStatusCode() + "");
+
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    // return it as a String
+                    String result = EntityUtils.toString(entity);
+                    Bukkit.getLogger().info(result);
+                    JsonObject input = NotBounties.serverVersion >= 18 ? JsonParser.parseString(result).getAsJsonObject() : new JsonParser().parse(result).getAsJsonObject();
+                    JsonObject country = input.getAsJsonObject("country");
+                    return country.getAsJsonObject("iso_code").getAsString();
+                } else {
+                    throw new RuntimeException("Did not get a result from request.");
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 
