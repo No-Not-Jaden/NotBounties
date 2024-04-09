8a0e495f580739b5d5061b28a16b6ec596166e08
Not_Jaden <jpater567@gmail.com>
2024-04-09 16:12:22 -0500

    finished local time

--- src/main/java/me/jadenp/notbounties/utils/externalAPIs/LocalTime.java
+++ src/main/java/me/jadenp/notbounties/utils/externalAPIs/LocalTime.java
@@ -15,7 +15,6 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
@@ -30,7 +29,7 @@ import java.util.concurrent.ExecutionException;
 public class LocalTime {
 
     // these codes will only be saved in RAM and will be deleted after the plugin is disabled
-    private static final Map<UUID, String> savedIsoCodes = new HashMap<>();
+    private static final Map<UUID, TimeZone> savedTimeZones = new HashMap<>();
     private static int account;
     private static String license = null;
 
@@ -54,26 +53,23 @@ public class LocalTime {
                 return formatTime(time);
             }
         }
-        if (savedIsoCodes.containsKey(player.getUniqueId()))
-            return formatTime(time, savedIsoCodes.get(player.getUniqueId()));
+        if (savedTimeZones.containsKey(player.getUniqueId()))
+            return formatTime(time, savedTimeZones.get(player.getUniqueId()));
         InetSocketAddress address = player.getAddress();
         if (address == null)
             return formatTime(time);
 
 
-        CompletableFuture<String> isoCode = getIsoCode(address);
-        Bukkit.getLogger().info("Requesting iso code: " + address);
+        CompletableFuture<TimeZone> timezone = getTimezone(address);
         try {
-            savedIsoCodes.put(player.getUniqueId(), isoCode.get());
-            Bukkit.getLogger().info(isoCode.get());
-            return formatTime(time, isoCode.get());
+            savedTimeZones.put(player.getUniqueId(), timezone.get());
+            return formatTime(time, timezone.get());
         } catch (ExecutionException | InterruptedException e) {
-            Bukkit.getLogger().info(e.toString());
             return formatTime(time);
         }
     }
 
-    private static CompletableFuture<String> getIsoCode(InetSocketAddress address) {
+    private static CompletableFuture<TimeZone> getTimezone(InetSocketAddress address) {
         if (license == null)
             return CompletableFuture.failedFuture(new RuntimeException("No Authentication Credentials!"));
         return CompletableFuture.supplyAsync(() -> {
@@ -92,16 +88,16 @@ public class LocalTime {
                  CloseableHttpResponse response = httpClient.execute(request)) {
 
                 // 401 if wrong user/password
-                Bukkit.getLogger().info(response.getStatusLine().getStatusCode() + "");
+                //Bukkit.getLogger().info(response.getStatusLine().getStatusCode() + "");
 
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
                     // return it as a String
                     String result = EntityUtils.toString(entity);
-                    Bukkit.getLogger().info(result);
                     JsonObject input = NotBounties.serverVersion >= 18 ? JsonParser.parseString(result).getAsJsonObject() : new JsonParser().parse(result).getAsJsonObject();
-                    JsonObject country = input.getAsJsonObject("country");
-                    return country.getAsJsonObject("iso_code").getAsString();
+                    JsonObject location = input.getAsJsonObject("location");
+
+                    return TimeZone.getTimeZone(location.get("time_zone").getAsString());
                 } else {
                     throw new RuntimeException("Did not get a result from request.");
                 }
@@ -112,17 +108,9 @@ public class LocalTime {
         });
     }
 
-    private static String formatTime(long time, String isoCode) {
-        for (Locale locale : Locale.getAvailableLocales()) {
-            if (locale.getISO3Country().equals(isoCode)) {
-                DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, locale);
-                dateFormat.setTimeZone(TimeZone.getTimeZone(isoCode));
-                return dateFormat.format(time);
-            }
-        }
-
+    private static String formatTime(long time, TimeZone timeZone) {
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, NumberFormatting.locale);
-        dateFormat.setTimeZone(TimeZone.getTimeZone(isoCode));
+        dateFormat.setTimeZone(timeZone);
         return dateFormat.format(time);
     }
 
