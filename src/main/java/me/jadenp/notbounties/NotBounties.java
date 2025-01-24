fadc8212bcfe8d6beb63bb276115cecfad23eac6
Not_Jaden <jpater567@gmail.com>
2025-01-23 23:54:39 -0600

    added error logs when the plugin can't read player data.

--- src/main/java/me/jadenp/notbounties/NotBounties.java
+++ src/main/java/me/jadenp/notbounties/NotBounties.java
@@ -150,6 +150,8 @@ public final class NotBounties extends JavaPlugin {
             DataManager.loadData(this);
         } catch (IOException e) {
             Bukkit.getLogger().severe("[NotBounties] Failed to read player data!");
+            Bukkit.getLogger().severe(e.toString());
+            Bukkit.getLogger().severe(Arrays.toString(e.getStackTrace()));
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
